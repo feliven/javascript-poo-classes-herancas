a31e121a62b2a598fbe4a25fdac73f6a60940f46
FVS <feliven@gmail.com>
2025-10-06 13:27:09 -0300

    level agora é privado

--- src/modules/Personagem.js
+++ src/modules/Personagem.js
@@ -2,29 +2,33 @@ export default class Personagem {
   nome;
   vida = 100;
   mana = 100;
-  level;
+  #level;
   static tipo;
   static descricao;
 
   constructor(nome, level, tipo, descricao) {
     this.nome = nome;
-    this.level = level;
+    this.#level = level;
     this.tipo = tipo;
     this.descricao = descricao;
   }
 
+  get level() {
+    return this.#level;
+  }
+
   obterInsignia() {
-    if (this.level >= 5) {
+    if (this.#level >= 5) {
       return `Implacável ${this.tipo}`;
     }
     return `${this.tipo} iniciante`;
   }
 
   static verificarVencedor(personagem1, personagem2) {
-    if (personagem1.level === personagem2.level) {
+    if (personagem1.#level === personagem2.#level) {
       return "Empate!";
     }
-    if (personagem1.level > personagem2.level) {
+    if (personagem1.#level > personagem2.#level) {
       return `${personagem1.constructor.tipo} ${personagem1.nome} ganhou!`;
     }
     return `${personagem2.constructor.tipo} ${personagem2.nome} ganhou!`;
