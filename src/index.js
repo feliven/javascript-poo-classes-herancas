195b95b2c3a9e815aff42cc71ce410a620b09df3
FVS <feliven@gmail.com>
2025-10-06 12:03:48 -0300

    criada classe ArqueiroEMago

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Personagem from "../src/modules/Personagem.js";
 import Mago from "../src/modules/Mago.js";
 import Arqueiro from "../src/modules/Arqueiro.js";
+import ArqueiroEMago from "./modules/ArqueiroEMago.js";
 import PersonagemView from "../src/components/personagem-view.js";
 
 // const personagemPedrinho = new Personagem("Pedrinho", 5, "Mago");
@@ -9,18 +10,30 @@ const personagemPedrinho = new Mago("Pedrinho", 5, "fogo", 4, 5);
 
 const personagemJose = new Personagem("José", 3, "Arqueiro");
 
-const personagemFelipe = new Mago("Felipe", 4, "água", 3, 2);
+const personagemFelipe = new Mago("Felipe", 4, "gelo", 3, 2);
 
 const arqueiroBruno = new Arqueiro("Bruno", 7, 8);
+
+const arqueiroMagoThiago = new ArqueiroEMago("Thiago", 3, 3, "ar", 4, 4);
 
 console.log(personagemPedrinho);
 console.log(personagemJose);
 console.log(personagemFelipe);
+console.log(arqueiroBruno);
+console.log(arqueiroMagoThiago);
 
 console.log(`Insígnia de ` + personagemPedrinho.nome + `: ` + personagemPedrinho.obterInsignia());
 console.log(`Insígnia de ` + personagemJose.nome + `: ` + personagemJose.obterInsignia());
 console.log(`Insígnia de ` + personagemFelipe.nome + `: ` + personagemFelipe.obterInsignia());
+console.log(`Insígnia de ` + arqueiroBruno.nome + `: ` + arqueiroBruno.obterInsignia());
+console.log(`Insígnia de ` + arqueiroMagoThiago.nome + `: ` + arqueiroMagoThiago.obterInsignia());
 
-const personagensExibidosNaTela = [personagemPedrinho, personagemJose, personagemFelipe, arqueiroBruno];
+const personagensExibidosNaTela = [
+  personagemPedrinho,
+  personagemJose,
+  personagemFelipe,
+  arqueiroBruno,
+  arqueiroMagoThiago,
+];
 
 new PersonagemView(personagensExibidosNaTela).render();
---
+++ src/modules/ArqueiroEMago.js
@@ -0,0 +1,22 @@
+import Personagem from "./Personagem.js";
+import Mago from "./Mago.js";
+import Arqueiro from "./Arqueiro.js";
+
+export default class ArqueiroEMago extends Personagem {
+  ladoArqueiro;
+  ladoMago;
+  elementoMagico;
+  levelMagico;
+  inteligencia;
+  tipo = "ArqueiroMago";
+
+  constructor(nome, level, destreza, elementoMagico, levelMagico, inteligencia) {
+    super(nome, level, "ArqueiroMago");
+    this.ladoArqueiro = new Arqueiro(nome, level, destreza);
+    this.ladoMago = new Mago(nome, level, elementoMagico, levelMagico, inteligencia);
+  }
+
+  obterInsignia() {
+    return `${this.ladoArqueiro.obterInsignia()} e ${this.ladoMago.obterInsignia()}`;
+  }
+}
